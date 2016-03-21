346e0abe25eb28476313fc3ec8e2907e4ff642fb
Daniel Stonier <d.stonier@gmail.com>
2016-03-21 17:56:23 +0900

    script for wrqt_py_trees

--- setup.py
+++ setup.py
@@ -6,6 +6,9 @@ from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup(
     packages=['rqt_wrapper'],
     package_dir={'': 'src'},
-    scripts=['scripts/wrqt_graph', 'scripts/wrqt_robot_monitor' ],
+    scripts=['scripts/wrqt_graph',
+             'scripts/wrqt_robot_monitor',
+             'scripts/wrqt_py_trees'
+             ],
 )
 setup(**d)
